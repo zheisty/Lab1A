d04efd54e49a84dcac83b0b5718ed5768007646e
zheisty <S01253938@acad.tri-c.edu>
2020-06-09 17:18:00 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,15 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+    string A = (" Apples=");
+            int Q = (10);
+            double P = (0.75);
+            string price = ("Total Price of");
+            string total = (price + A);
+
+            
+            Console.Write(total);
+            Console.Write(Q * P);
+  }
+}
