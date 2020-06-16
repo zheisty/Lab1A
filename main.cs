260341b5c0dddf9b021f9903cc64dadf77f7ab22
zheisty <S01253938@acad.tri-c.edu>
2020-06-16 01:31:51 +0000

    Added variables Item, quantity and price per 1 apple

--- main.cs
+++ main.cs
@@ -2,14 +2,35 @@ using System;
 
 class MainClass {
   public static void Main (string[] args) {
-    string A = (" Apples=");
+
+    string A = (" Apples= ");
+
             int Q = (10);
+
             double P = (0.75);
+
             string price = ("Total Price of");
+
             string total = (price + A);
 
-            
-            Console.Write(total);
-            Console.Write(Q * P);
+            string what = ("Item= ");
+
+            string p = ("Apple");
+
+            string q = ("Quantity of Apples= ");
+
+            string Pp = ("Price per Apple= ");
+
+
+
+            Console.WriteLine(what + p);
+
+            Console.WriteLine(q +Q);
+
+            Console.WriteLine(Pp + "$"+P);
+
+            Console.WriteLine(total + "$"+Q * P);
+
+
   }
 }
